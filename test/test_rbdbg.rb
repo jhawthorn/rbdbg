2533c847f1cf75e118890920b4f6b8da88165acc
John Hawthorn <john@hawthorn.email>
2022-03-20 23:30:35 -0700

    Consistent tests??

--- test/test_rbdbg.rb
+++ test/test_rbdbg.rb
@@ -11,14 +11,16 @@ class TestRbdbg < RbdbgTest
 
   def test_ptrace_attach
     child_pid = spawn("sleep", "1")
+    sleep 0.1
+
     Rbdbg.ptrace_attach(child_pid)
     pid, status = Process.waitpid2(child_pid)
     assert_equal pid, child_pid
     assert status.stopped?
     refute status.exited?
 
-    # SIGTRAP
-    #assert_equal 5, status.stopsig, "expected SIGTRAP but got #{status.inspect}"
+    # SIGSTOP
+    assert_equal 19, status.stopsig, "expected SIGSTOP but got #{status.inspect}"
   end
 
   def test_attach_pid
@@ -29,20 +31,15 @@ class TestRbdbg < RbdbgTest
 
   def test_ptrace_detach
     child_pid = spawn("sleep", "1")
+    sleep 0.1
     Rbdbg.attach_pid(child_pid)
 
     Rbdbg.ptrace_detach(child_pid)
 
     # FIXME
-    #_, status = Process.waitpid2(child_pid)
-    #p status
+    _, status = Process.waitpid2(child_pid)
+    p status
     #assert status.exited?
-  end
-
-  def test_attach_pid
-    child_pid = spawn("sleep", "1")
-    debugee = Rbdbg.attach_pid(child_pid)
-    p debugee
   end
 
   def test_registers
